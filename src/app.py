966007bf9dfe127f5fe0590fe8371e91235202ee
EC2 Default User <ec2-user@ip-172-31-5-177.ap-south-1.compute.internal>
2025-12-25 11:43:34 +0000

    samll change

--- src/app.py
+++ src/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "CICD USING JENKINS  EXECUTED SUCCESSFULLY"
+    return "CICD USING JENKINS and EXECUTED SUCCESSFULLY"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
